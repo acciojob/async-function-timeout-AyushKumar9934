d336723fe8bea3fe6dc74ced0dd23ae5c6815afd
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-17 18:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const handleClick=()=>{
+  let input=document.getElementById("text").value;
+
+let value=document.getElementById("delay").value;
+  console.log("input",input);
+  console.log("value",value)
+  setTimeout(()=>{
+    document.getElementById("output").textContent=`${input}`
+
+  },parseInt(value))
+}
+document.getElementById("btn").addEventListener("click",handleClick);
